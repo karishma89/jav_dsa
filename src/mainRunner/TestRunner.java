d2d864d63ca6f92cbfec9cf82034137eb55aea7c
karishma89 <kamya.saman@gmail.com>
2023-06-05 01:24:25 -0700

    runner

---
+++ src/mainRunner/TestRunner.java
@@ -0,0 +1,12 @@
+package mainRunner;
+
+import patternProgramming.SolvePatternWithTricks;
+
+public class TestRunner {
+
+public static void main(String[] args) {
+		
+		SolvePatternWithTricks.pattern4(4);
+	}
+
+}
